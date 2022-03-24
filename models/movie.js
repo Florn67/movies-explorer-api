94cee2c205b59d47e31eb497cfc4b5e7e4eadc8d
Florn67 <edgewalker909@gmail.com>
2022-03-24 12:40:13 +0300

    fix nameRu

--- models/movie.js
+++ models/movie.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const uniqueValidator = require('mongoose-unique-validator');
 function validateUrl(url) {
   return validator.isURL(url);
 }
@@ -52,12 +51,11 @@ const movieSchema = new mongoose.Schema({
   nameRU: {
     type: String,
     required: true,
-    unique: true,
   },
   nameEN: {
     type: String,
     required: true,
   },
 });
-movieSchema.plugin(uniqueValidator);
+
 module.exports = mongoose.model('movie', movieSchema);
